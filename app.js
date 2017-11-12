766dbf823b28b0ff67ea65f4864255f05e4a327d
sKiL <skil@skildust.com>
2017-11-12 15:21:47 +0100

    Fix time displaying wrong cause I'm just too stupid

--- app.js
+++ app.js
@@ -131,9 +131,9 @@ function player (guildID) {
 }
 
 function secondsToTimeString (duration) {
-  let hours = Math.floor(duration / 1200)
-  let minutes = Math.floor(duration / 60)
-  let seconds = duration - (hours * 1200) - (minutes * 60)
+  let hours = Math.floor(duration / 3600)
+  let minutes = Math.floor((duration - (hours * 3600)) / 60)
+  let seconds = duration - (hours * 3600) - (minutes * 60)
 
   hours = hours >= 10 ? hours : '0' + hours
   minutes = minutes >= 10 ? minutes : '0' + minutes
